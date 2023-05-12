a3505d383dce211cdf8c48a504d374e3546e8fde
ajr250571 <ajr250571@gmail.com>
2023-05-12 17:03:08 -0300

    Last Update

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from .models import MeasureUnit, CategoryProduct, Indicator, Product
 
+
 class MeasureUnitAdmin(admin.ModelAdmin):
-    list_display = ('id','description')
+    list_display = ('id', 'description')
+    ordering = ('id',)
 
 class CategoryProductAdmin(admin.ModelAdmin):
-    list_display = ('id','description')
+    list_display = ('id', 'description')
+    ordering = ('id',)
 
-admin.site.register(MeasureUnit,MeasureUnitAdmin)
-admin.site.register(CategoryProduct,CategoryProductAdmin)
+
+admin.site.register(MeasureUnit, MeasureUnitAdmin)
+admin.site.register(CategoryProduct, CategoryProductAdmin)
 admin.site.register(Indicator)
 admin.site.register(Product)
